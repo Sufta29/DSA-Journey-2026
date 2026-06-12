1083ce7f29887366cd0dc0915b14ffffe2362f17
Sufta29 <sufta_2501mc29@iitp.ac.in>
2026-06-12 22:34:09 +0530

    Add Pattern 03 solution

---
+++ Patterns/pattern_03.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+/*
+Striver A2Z Sheet
+Pattern 01
+1
+12
+123
+1234
+12345
+*/
+
+// solution
+class Solution {
+public:
+    void pattern3(int n) {
+for(int i= 1;i<=n;i++){
+     for(int j =1; j<=i;j++){
+        cout<<j;
+     }
+     cout<<endl;
+}
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+
+    Solution obj;
+    obj.pattern3(n);
+
+    return 0;
+}
